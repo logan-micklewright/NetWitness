88d44d8fa79a03f382c65adc928a9e63074143ef
Logan <logan.micklewright@gmail.com>
2020-11-25 09:08:50 -0800

    Add get-parsers commandlet

--- Get-Parsers.ps1
+++ Get-Parsers.ps1
@@ -2,10 +2,11 @@ Import-Module .\NetWitness-Ops
 
 $apiCreds = Get-Credential -Message "Enter current Admin password to connect" -UserName "admin"
 
-$decoders = Get-NWDecoders -NWHost "10.178.203.182"
+$nwServer = Read-Host -Prompt "Enter hostname or IP of NW server to query"
+$decoders = Get-NWDecoders -NWHost $nwServer
 
 $parsers = Get-Parsers -apiCreds $apiCreds -NWServices $decoders
 
-$parsers | $par-Csv -Path ./parsers.csv
+$parsers | Out-Csv -Path ./parsers.csv
 
 $parsers | Out-file -Path ./parsers.txt
