fa935ee6bdaf6dcf4a82974c5c40d21a8d03855c
Logan <logan.micklewright@gmail.com>
2021-02-19 19:37:11 -0800

    Improvements to Ops module and server build
    Added significantly more steps into the unified server build script and built a number of different functions in the Ops module to support them. Also improved the usability of the Ops module as a standalone tool

--- NetWitness-Ops/NetWitness-Ops.psm1
+++ NetWitness-Ops/NetWitness-Ops.psm1
@@ -91,10 +91,10 @@ function Get-NWDecoders{
         [Parameter(Mandatory=$true, ValueFromPipeline=$true, HelpMessage="Enter the hostname or IP of the NW head unit to query for available hosts")]
         [String] $NWHost
     )
-    $decoders = Get-NWServices -NWHost $NWHost | Where name -eq "Decoder"
+    $decoders = Get-NWServices -NWHost $NWHost | Where-Object -Property name -eq "Decoder"
     $NWHosts = Get-NWHosts -NWHost $NWHost
     foreach ($dec in $decoders) { 
-        $dec.name = $NWHosts | Where {$_.IP -eq $dec.IP} | Select-Object -ExpandProperty Name 
+        $dec.name = $NWHosts | Where-Object -FilterScript {$_.IP -eq $dec.IP} | Select-Object -ExpandProperty Name 
     }
     return $decoders
 }
@@ -178,7 +178,7 @@ function Set-NWRoles{
         [Parameter(Mandatory=$true, HelpMessage="Roles to apply, this should be from the output of Get-NWRoles")]
         [System.Object] $roles
     )
-    $builtInRoles = "Administrators", "Aggregation", "Analysts", "Data_Privacy_Officers", "Malware_Analysts", "Operators", "SOC_Managers"
+    $builtInRoles = "Administrators", "Aggregation", "Analysts", "Data_Privacy_Officers", "Malware_Analysts", "Operators", "SOC_Managers", "Reporting_Engine_Content_Administrators"
     $destinationHost = $NWService.IP
     $port = $NWService.Port
     $baseURI = "https://$destinationHost"+":"+$port
@@ -208,7 +208,7 @@ function Copy-NWRoles{
         [string]$port
     )
     
-    $builtInRoles = "Administrators", "Aggregation", "Analysts", "Data_Privacy_Officers", "Malware_Analysts", "Operators", "SOC_Managers"
+    $builtInRoles = "Administrators", "Aggregation", "Analysts", "Data_Privacy_Officers", "Malware_Analysts", "Operators", "SOC_Managers", "Reporting_Engine_Content_Administrators"
     $baseRefURI = "https://$referenceHost"+":"+$port
     $baseURI = "https://$destinationHost"+":"+$port
 
@@ -245,7 +245,7 @@ function Get-Parsers {
         $result = Invoke-RestMethod -Uri "$serviceURI" -Credential $apiCreds -SkipCertificateCheck
         foreach ($node in $result.nodes){
             if (($node.name -ne "uuid") -and ($node.name -ne "dateInstalled")){
-                $parsers = $result.nodes | Where {($_.name -ne "uuid") -and ($_.name -ne "dateInstalled")} | Select name
+                $parsers = $result.nodes | Where-Object -FilterScript {($_.name -ne "uuid") -and ($_.name -ne "dateInstalled")} | Select-Object -Property name
                 $output[$name] = $parsers.name
             }
         }
@@ -254,3 +254,283 @@ function Get-Parsers {
 return $output
 }
 
+function Set-NWStorage {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, HelpMessage="Provide a credential object used to authenticate to the API")]
+        [pscredential] $apiCreds,
+        [Parameter(Mandatory=$true, HelpMessage="Provide the IP of the host to configure")]
+        [string]$nodeIP,
+        [Parameter(Mandatory=$true, HelpMessage="Provide the type of service on the host")]
+        [string]$serviceType
+    )
+
+    $appliancePort="50106"
+    $concentratorPort="50105"
+    $decoderPort="50104"
+    $logdecoderPort="50102"
+
+    #$endpointToConfig = Read-Host -Prompt "Enter the IP of the host you want to configure"
+    #$service = Read-Host -Prompt "Enter the type of host this is"
+    $endpointToConfig = $nodeIP
+    $service = $serviceType
+    if($service -ne "concentrator" -And $service -ne "decoder" -And $service -ne "logdecoder"){
+        Write-Host "Service Type must be either concentrator, decoder or logdecoder"
+        return
+    }
+
+    $apiCreds = Get-Credential -Message "Enter current Admin password to connect" -UserName "admin"
+
+    #List storage controllers/enclosures
+    $raidList = "/appliance?msg=raidList&force-content-type=application/json"
+    $apiURI = "https://$endpointToConfig"+":"+$appliancePort+$raidList
+    $controllers = Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+
+    <#NW api returns data as a bunch of raw text, even when selecting json as the return type it's just a big string, example:
+    Controller 0 at PCI Address 18:00.0, Enclosure 64
+        Vendor:  DP
+        Model:   BP14G+EXP
+        In Use:  true
+        Drives:  931.512 GB HDD x 2
+                1.819 TB HDD x 2
+        Devices: sda /dev/disk/by-path/pci-0000:18:00.0-scsi-0:2:0:0
+                sdb /dev/disk/by-path/pci-0000:18:00.0-scsi-0:2:1:0
+
+    Controller 1 at PCI Address 3b:00.0, Enclosure 250
+        Vendor:  DELL
+        Model:   MD1400
+        In Use:  true
+        Drives:  10.692 TB HDD x 12
+        Devices: sdc /dev/disk/by-path/pci-0000:3b:00.0-scsi-0:2:0:0
+                sdd /dev/disk/by-path/pci-0000:3b:00.0-scsi-0:2:1:0
+
+    So we do a couple things here, first trim the whitespace at the beginning and end
+    Then split on blank lines since there's a blank line between each enclosure, so now we have chunks of properties we can work with
+    Loop through those chunks and take the first line which contains controller and enclosure number, ex: Controller 1 at PCI Address 3b:00.0, Enclosure 250 use a regex to replace the junk in the middle we don't care about so we get Controller 1, Enclosure 250 
+    Split that at the comma to get Controller 1 and Enclosure 250 onto separate lines and now it matches the format of the other properties
+    Finally replace the : with = in each line so that we can pass the whole thing to ConvertFrom-StringData and get something pretty and usable
+    #>
+    $controllers = $controllers.string.trim()
+    $controllers = $controllers.Split("`n`n")
+    foreach ($controller in $controllers){
+        $controllerInfo = (($controller -split '\n')[0] -replace "\sat.*,",",").Split(", ")
+        $controller = $controller.Substring($controller.IndexOf('Vendor'))
+        $controller = $controller -replace "             .*"
+        $controller = ($controllerInfo -replace " ","=") + ($controller -replace "Devices:","Devices: ")
+        $controller =  $controller -replace ': ','=' | ConvertFrom-StringData
+        #If the enclosure is already in use don't touch it, only configure unused enclosures, the api will fail the request anyway if it's in use but I don't trust that
+        if ($controller.'In Use' -eq "false"){
+            $enclosure = $controller.Enclosure
+            $number = $controller.Controller
+            $raidNew = "/appliance?msg=raidNew&force-content-type=text/plain&controller=$number&enclosure=$enclosure&scheme=$service&commit=1"
+            $apiURI = "https://$endpointToConfig"+":"+$appliancePort+$raidNew
+            Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+        }
+    }
+
+    #Now that we have created raids on each enclosure, get a list of the storage devices available so we can partition them
+    $devList = "/appliance?msg=devlist&force-content-type=text/plain"
+    $apiURI = "https://$endpointToConfig"+":"+$appliancePort+$devList
+    $devices = Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+
+    $devices = $devices.trim()
+    $devices = $devices.Split("`n")
+
+    $storageDevices = @()
+    foreach($device in $devices){
+        $deviceName = "name="+$device.Substring(0,3)
+        $deviceSize = $device.Substring($device.IndexOf("size"))
+        $deviceSize = $deviceSize -replace '="','='
+        $deviceSize = ($deviceSize -split '"')[0]
+        $deviceUsed = $device.Substring($device.IndexOf("used"))
+
+        $deviceProps = $deviceName + "`n" + $deviceSize + "`n" + $deviceUsed
+        $deviceProps = $deviceProps | ConvertFrom-StringData
+
+        if($deviceProps.used -eq "0"){
+            $sd = New-Object -TypeName psobject
+            $sd | Add-Member -MemberType Noteproperty -Name DeviceName -Value $deviceProps.name
+            $sd | Add-Member -MemberType Noteproperty -Name Size -Value $deviceProps.size
+            $sd | Add-Member -MemberType Noteproperty -Name Used -Value $deviceProps.used
+            $storageDevices+= $sd
+        }
+    }
+
+    $storageDevices
+
+    #Decoders and log decoders both have a simliar layout, decodersmall volume and then decoder volume, where decodersmall must be partitioned and allocated first
+    #Concentrators follow a different pattern with index and concentrator volumes where index is the smaller volume and must be partitioned second but allocated first
+    if($service -eq "decoder" -Or $service -eq "logdecoder"){
+        #First thing to check is how many storage devices we have, which should always be an even number otherwise we have mismatched drives
+        if($storageDevices.Length%2 -eq 0){
+            #If we sort by size of the storage device the first half of the devices will be the decodersmall volumes, second half (or first half if sorted descending) will be decoder volumes
+            $decoderSmall = $storageDevices | Sort-Object -Property Size | Select-Object DeviceName -First $storageDevices.Length/2
+            $decoder = $storageDevices | Sort-Object -Property Size | Select-Object DeviceName -Last $storageDevices.Length/2
+
+            foreach($sd in $decoderSmall){
+                $name=$sd.DeviceName
+                $apiURI = "https://$endpointToConfig"+":"+$appliancePort+"/appliance?msg=partNew&force-content-type=text/plain&name=$name&service=$service&volume=$service"+"small&commit=1"
+                Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+            }
+            foreach($sd in $decoder){
+                $name=$sd.DeviceName
+                $apiURI = "https://$endpointToConfig"+":"+$appliancePort+"/appliance?msg=partNew&force-content-type=text/plain&name=$name&service=$service&volume=$service&commit=1"
+                Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+            }
+            Start-Sleep -s 5
+
+            #Once storage devices are partitioned they can then be allocated to the services
+            $apiURI="https://$endpointToConfig"+":"+$appliancePort+"/appliance?msg=srvAlloc&force-content-type=text/plain&service=$service&volume=$service"+"small&commit=1"
+            Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+            Start-Sleep -s 2
+            $i=0
+            #Storage volumes start with just the volume name eg decodersmall, then subsequent devices with the same volume get a number starting with 0
+            #So if you have for example 4 decodersmall volumes they would be decodersmall, decodersmall0, decodersmall1, and decodersmall2
+            #This means if there are 8 total storage devices then length/2 is 4 but we need just 0, 1, and 2
+            while($i -lt (($storageDevices.Length/2)-1)){
+                $apiURI="https://$endpointToConfig"+":"+$appliancePort+"/appliance?msg=srvAlloc&force-content-type=text/plain&service=$service&volume=$service"+"small"+$i+"&commit=1"
+                Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+                Start-Sleep -s 5
+                $i++
+            }        
+            $apiURI="https://$endpointToConfig"+":"+$appliancePort+"/appliance?msg=srvAlloc&force-content-type=text/plain&service=$service&volume=$service&commit=1"
+            Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+            Start-Sleep -s 2
+            $i=0
+            while($i -lt (($storageDevices.Length/2)-1)){
+                $apiURI="https://$endpointToConfig"+":"+$appliancePort+"/appliance?msg=srvAlloc&force-content-type=text/plain&service=$service&volume=$service"+$i+"&commit=1"
+                Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+                Start-Sleep -s 10
+                $i++
+            }
+        }else{
+            Write-Host "Invalid drive configuration, manual configuration required"
+            return
+        }
+    }
+
+    if($service -eq "concentrator"){
+        if($storageDevices.Length%2 -eq 0){
+            $index = $storageDevices | Sort-Object -Property Size | Select-Object DeviceName -First $storageDevices.Length/2
+            $concentrator = $storageDevices | Sort-Object -Property Size | Select-Object DeviceName -Last $storageDevices.Length/2
+            foreach($sd in $concentrator){
+                $name=$sd.DeviceName
+                $apiURI = "https://$endpointToConfig"+":"+$appliancePort+"/appliance?msg=partNew&force-content-type=text/plain&name=$name&service=$service&volume=concentrator&commit=1"
+                Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+            }
+            foreach($sd in $index){
+                $name=$sd.DeviceName
+                $apiURI = "https://$endpointToConfig"+":"+$appliancePort+"/appliance?msg=partNew&force-content-type=text/plain&name=$name&service=$service&volume=index&commit=1"
+                Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+            }
+            Start-Sleep -s 5
+            #Once storage devices are partitioned they can then be allocated to the services
+            $apiURI="https://$endpointToConfig"+":"+$appliancePort+"/appliance?msg=srvAlloc&force-content-type=text/plain&service=$service&volume=index&commit=1"
+            Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+            Start-Sleep -s 2
+            $i=0
+            while($i -lt (($storageDevices.Length/2)-1)){
+                $apiURI="https://$endpointToConfig"+":"+$appliancePort+"/appliance?msg=srvAlloc&force-content-type=text/plain&service=$service&volume=index$i&commit=1"
+                Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+                Start-Sleep -s 5
+                $i++
+            }
+            $apiURI="https://$endpointToConfig"+":"+$appliancePort+"/appliance?msg=srvAlloc&force-content-type=text/plain&service=$service&volume=concentrator&commit=1"
+            Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+            Start-Sleep -s 2
+            while($i -lt (($storageDevices.Length/2)-1)){
+                $apiURI="https://$endpointToConfig"+":"+$appliancePort+"/appliance?msg=srvAlloc&force-content-type=text/plain&service=$service&volume=concentrator$i&commit=1"
+                Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+                Start-Sleep -s 10
+                $i++
+            }
+        }else{
+            Write-Host "Invalid drive configuration, manual configuration required"
+            return
+        }
+    }
+
+    if($service -eq "decoder"){    
+        #Finally once the storage has been allocated we need to update config values for the service
+        $apiURI="https://$endpointToConfig"+":"+$decoderPort+"/decoder?msg=reconfig&force-content-type=text/plain&update=1"
+        Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+        Start-Sleep -s 2
+        $apiURI="https://$endpointToConfig"+":"+$decoderPort+"/database?msg=reconfig&force-content-type=text/plain&update=1"
+        Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+    }
+    if($service -eq "logdecoder"){
+        #Finally once the storage has been allocated we need to update config values for the service
+        $apiURI="https://$endpointToConfig"+":"+$logdecoderPort+"/decoder?msg=reconfig&force-content-type=text/plain&update=1"
+        Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+        Start-Sleep -s 2
+        $apiURI="https://$endpointToConfig"+":"+$logdecoderPort+"/database?msg=reconfig&force-content-type=text/plain&update=1"
+        Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+    }
+
+    if($service -eq "concentrator"){
+        #Finally once the storage has been allocated we need to update config values for the service
+        $apiURI="https://$endpointToConfig"+":"+$concentratorPort+"/concentrator?msg=reconfig&force-content-type=text/plain&update=1"
+        Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+        Start-Sleep -s 2
+        $apiURI="https://$endpointToConfig"+":"+$concentratorPort+"/database?msg=reconfig&force-content-type=text/plain&update=1"
+        Invoke-RestMethod -Uri "$apiURI" -Credential $apiCreds -SkipCertificateCheck
+    }
+}
+
+function New-NWHost {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, HelpMessage="Provide the name of the host to add")]
+        [string]$NWHostName,
+        [Parameter(Mandatory=$true, HelpMessage="Provide the IP of the host to add")]
+        [string]$nodeIP,
+        [Parameter(Mandatory=$true, HelpMessage="Provide the IP of the head unit")]
+        [string]$NWHeadUnit
+    )
+    
+    $rawSaltInfo = ssh root@$nodeIP cat /etc/salt/minion
+
+    #TODO Parse the raw salt info to get the actual minion ID
+    $minionID = $rawSaltInfo
+
+    ssh root@$NWHeadUnit orchestration-cli-client --register-host -d $minionID -n $NWHostName -o $nodeIP --ipv4 $nodeIP
+}
+
+function New-NWService {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, HelpMessage="Provide the type of service to install")]
+        [string]$ServiceType,
+        [Parameter(Mandatory=$true, HelpMessage="Provide the IP of the host to add the service to")]
+        [string]$nodeIP,
+        [Parameter(Mandatory=$true, HelpMessage="Provide the IP of the head unit")]
+        [string]$NWHeadUnit
+    )
+    
+    $rawSaltInfo = ssh root@$nodeIP cat /etc/salt/minion
+
+    #TODO Parse the raw salt info to get the actual minion ID
+    $minionID = $rawSaltInfo
+
+    ssh root@$NWHeadUnit orchestration-cli-client --install -c $ServiceType -o $minionID
+}
+
+function Update-NWHost {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, HelpMessage="Provide the IP of the host to update")]
+        [string]$nodeIP,
+        [Parameter(Mandatory=$true, HelpMessage="Provide the IP of the head unit")]
+        [string]$NWHeadUnit
+    )
+    
+    $rawSaltInfo = ssh root@$nodeIP cat /etc/salt/minion
+
+    #TODO Parse the raw salt info to get the actual minion ID
+    $minionID = $rawSaltInfo
+
+    $currentVersion = ssh root@$NWHeadUnit upgrade-cli-client --list | grep AUSPLNWHU
+    $currentVersion = $currentVersion.Substring($currentVersion.length-5)
+
+    ssh root@$NWHeadUnit upgrade-cli-client -u --host-key $minionID --version $currentVersion
+}
--- ServerBuild.ps1
+++ ServerBuild.ps1
@@ -4,39 +4,109 @@ Import-Module .\iDrac-Ops
 #Very much still a work in progress. I laid out the various sections that would be needed, most of them are implemented in stand-alone scripts I've already written, but I hope to pull it all together at some point
 
 #Get a list of servers to build, prompt user to choose individual IP address or csv file
+function Show-IPChoices {
+    $Title = 'NetWitness Server Build Tool'
+    Clear-Host
+    Write-Host "================ $Title ================"
+    
+    Write-Host "1: Single IP."
+    Write-Host "2: List of IPs from file"
+}
 
+Add-Type -AssemblyName System.Windows.Forms
+
+#If importing from file the csv MUST have a column titled IP and a column titled Name, additional columns are allowed but ignored by the script
+#If user enters a single IP the script builds it into a PSObject simply to match the format that is created when a csv is imported, making the rest of the script simpler since is it always working off the same format of input data
+Show-IPChoices
+$selection = Read-Host "Please make a selection"
+switch ($selection) {
+    '1' {
+        $IP = Read-Host "IP Address"
+        $MachineName = Read-Host "Host Name"
+        $IPAddresses = New-Object -TypeName psobject
+        $IPAddresses | Add-Member -MemberType NoteProperty -Name IP -Value $IP
+        $IPAddresses | Add-Member -MemberType NoteProperty -Name Name -Value $MachineName
+    }
+    '2' {
+        $FileChooser = New-Object -TypeName System.Windows.Forms.OpenFileDialog
+        $FileChooser.InitialDirectory = [Environment]::GetFolderPath('Desktop')
+        $FileChooser.filter = "Comma Seperated Value (*.csv)| *.csv"
+        $FileChooser.Title = "Choose file containing names and IP addresses"
+        $FileChooser.ShowDialog()
+        $FilePath = $FileChooser.Filename
+        $IPAddresses = Import-Csv $FilePath
+    }
+}
 
 #Establish some baseline values, we don't want to bug the user after this point, should be an unattended, automated, build process
+$NWHeadUnit = Read-Host -Prompt "Enter the IP address of the NetWitness Head Unit"
+$apiCreds = Get-Credential -Message "Enter default Admin password to connect" -UserName "admin"
+$newCreds = Get-Credential -Message "Enter desired new admin password to set during build" -UserName "admin"
 
-#Now we're going to start interacting with the NW API for the remainder of the config so let's get some creds to work with
-$apiCreds = Get-Credential -Message "Enter current Admin password to connect" -UserName "admin"
+$NWHosts = Get-NWHosts -NWHost $NWHeadUnit
+$NWServices = Get-NWServices -NWHost $NWHeadUnit
 
-#Prompt the user to select a reference broker, concentrator, decoder, and logdecoder
-$refBroker = $NWServices | Where-Object {$_.Name -eq "broker"} | Out-GridView -PassThru -Title 'Select Reference Broker'
-$brokerRoles = Get-NWRoles -NWService $refBroker -apiCreds $apiCreds
+#Prompt the user to select which service types they're building and select a reference broker, concentrator, decoder, and logdecoder as needed
+$title    = 'Choose'
+$question = 'Are you configuring any Broker services?'
+$choices  = '&Yes', '&No'
 
-$refConcentrator = $NWServices | Where-Object {$_.Name -eq "concentrator"} | Out-GridView -PassThru -Title 'Select Reference Concentrator'
-$concentratorRoles = Get-NWRoles -NWService $refConcentrator -apiCreds $apiCreds
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+if ($decision -eq 0) {
+    $refBroker = $NWServices | Where-Object {$_.Name -eq "broker"} | Out-GridView -PassThru -Title 'Select Reference Broker'
+    $brokerRoles = Get-NWRoles -NWService $refBroker -apiCreds $apiCreds
+} else {
+    Write-Host 'No Brokers'
+}
 
-$refDecoder = $NWServices | Where-Object {$_.Name -eq "decoder"} | Out-GridView -PassThru -Title 'Select Reference Decoder'
-$decoderRoles = Get-NWRoles -NWService $refDecoder -apiCreds $apiCreds
+$question = 'Are you configuring any Concentrator services?'
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+if ($decision -eq 0) {
+    $refConcentrator = $NWServices | Where-Object {$_.Name -eq "concentrator"} | Out-GridView -PassThru -Title 'Select Reference Concentrator'
+    $concentratorRoles = Get-NWRoles -NWService $refConcentrator -apiCreds $apiCreds
+} else {
+    Write-Host 'No Concentrators'
+}
 
-$refLogDecoder = $NWServices | Where-Object {$_.Name -eq "logdecoder"} | Out-GridView -PassThru -Title 'Select Reference Log Decoder'
-$logDecoderRoles = Get-NWRoles -NWService $refLogDecoder -apiCreds $apiCreds
+$question = 'Are you configuring any Decoder services?'
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+if ($decision -eq 0) {
+    $refDecoder = $NWServices | Where-Object {$_.Name -eq "decoder"} | Out-GridView -PassThru -Title 'Select Reference Decoder'
+    $decoderRoles = Get-NWRoles -NWService $refDecoder -apiCreds $apiCreds
+} else {
+    Write-Host 'No Decoders'
+}
+
+$question = 'Are you configuring any Log Decoder services?'
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+if ($decision -eq 0) {
+    $refLogDecoder = $NWServices | Where-Object {$_.Name -eq "logdecoder"} | Out-GridView -PassThru -Title 'Select Reference Log Decoder'
+    $logDecoderRoles = Get-NWRoles -NWService $refLogDecoder -apiCreds $apiCreds
+} else {
+    Write-Host 'No Log Decoders'
+}
 
 #Call iDrac setup script, should handle everything up through mounting the ISO, pass the list of IPs to it
 
 #At this stage the server boots and the image is installed, it takes an hour, maybe figure out a way for the script to check status every x minutes and then proceed once imaging is complete
 
-#Now the OS needs to be configured, I haven't yet automated most of those tasks, on my todo list, so pause with the option to resume once OS has been configured
+#Now the OS needs to be configured, this is mostly handled by Salt but the nwsetup-tui must be called. The catch is that the IP address isn't set yet, so usually we do this through the iDRAC remote console, how to automate?
 
 #Then we're into the NW config, push services with orchestration client, install updates. Once services are installed, build out the objects to use for the rest of the config
+foreach($server in $NewHosts){
+    New-NWHost -NWHeadUnit $NWHeadUnit -nodeIP $server.IP -NWHostName - $server.Name
+}
+foreach($service in $NewServices){
+    New-NWService -serviceType $service.Name -nodeIP $service.IP -NWHeadUnit $NWHeadUnit
+    Update-NWHost -nodeIP $service.IP -NWHeadUnit $NWHeadUnit
+}
+
 
 #Enable SSL on the rest api
-foreach($service in $NWServices){
+foreach($service in $NewServices){
     Set-NWSSL -NWService $service -apiCreds $apiCreds    
 }
-foreach($server in $NWHosts){
+foreach($server in $NewHosts){
     $service = New-Object -TypeName psobject
     $service | Add-Member -MemberType Noteproperty -Name Name -Value "Appliance"
     $service | Add-Member -MemberType Noteproperty -Name IP -Value $server.IP
@@ -45,11 +115,11 @@ foreach($server in $NWHosts){
 }
 
 #Change the admin password
-$newCreds = Get-Credential -Message "Enter desired new admin password" -UserName "admin"
-foreach($service in $NWServices){
+
+foreach($service in $NewServices){
     Set-NWPassword -NWService $service -apiCreds $apiCreds -newCreds $newCreds    
 }
-foreach($server in $NWHosts){
+foreach($server in $NewHosts){
     $service = New-Object -TypeName psobject
     $service | Add-Member -MemberType Noteproperty -Name Name -Value "Appliance"
     $service | Add-Member -MemberType Noteproperty -Name IP -Value $server.IP
@@ -61,8 +131,7 @@ foreach($server in $NWHosts){
 $apiCreds = $newCreds
 
 #Replicate roles
-
-foreach($service in $NWServices){
+foreach($service in $NewServices){
     if($service.Name -eq "broker"){
         $roles = $brokerRoles
     }elseif($service.Name -eq "concentrator"){
@@ -79,7 +148,16 @@ foreach($service in $NWServices){
 
 
 #Configure storage
+foreach($service in $NewServices){
+    Set-NWStorage -nodeIP $service.IP -serviceType $service.Name -apiCreds $apiCreds
+}
+
 
 #Configure aggregation/capture
+
+#If decoder or log decoder, install warehouse connector
+
+#Install InsightVM Agent
+
 
 #Produce a verification report for all hosts once the build is complete
