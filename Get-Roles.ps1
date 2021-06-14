8a566c21b3dfbd6aefaf10acdfbd0cb0e3a1cf03
Logan <logan.micklewright@gmail.com>
2021-06-14 14:11:51 -0700

    Optimize Get-Roles
    Removed the need for the separate bash script and javascript file on the NetWitness side as well as the additional role with all enabled permissions.
    New version enumerates the list of all permissions from a different collection in Mongo and then uses that list directly to compare against all roles.

--- Get-Roles.ps1
+++ Get-Roles.ps1
@@ -1,18 +1,16 @@
-#IMPORTANT: This relies on the presence of the getRoles.sh and getRoles.js scripts on the NetWitness server or it will not return any data
+
 
 $mongoCreds = Get-Credential -Message "Enter Mongo password" -UserName "deploy_admin"
 $mongoPass = $mongoCreds.GetNetworkCredential().Password
 
 $headUnit = Read-Host -Prompt "Enter the IP of the head unit"
 
-ssh root@$headunit "/root/getRoles.sh $mongoPass"
-scp root@${headunit}:/root/NetWitnessRoles.json ./
-
-$rawRoles = Get-Content -Path NetWitnessRoles.json
+$rawPermissions = ssh root@$headUnit "mongo security-server -u deploy_admin -p $mongoPass --authenticationDatabase admin --quiet --eval \`"printjson( db.getCollection(`'permission`').find({},{ _id: 1 }).toArray() )\`""
+$rawRoles = ssh root@$headUnit "mongo security-server -u deploy_admin -p $mongoPass --authenticationDatabase admin --quiet --eval \`"printjson(  db.getCollection('role').find({  }, { _id: 1, permissions: 1}).toArray() )\`""
 
 $roles = $rawRoles | ConvertFrom-Json
 
-$permissionsList = $roles | Where-Object {$_._id -eq 'All-Permissions-Not-Used'} | Select-Object -ExpandProperty permissions
+$permissionsList = $rawPermissions | ConvertFrom-Json | Select-Object -ExpandProperty _id
 
 $formattedRoles = @()
 
@@ -20,12 +18,9 @@ foreach ($role in $roles){
     $permissionObject = [PSCustomObject]@{
         Name = $role._id
     }
-    foreach ($permission in $permissionsList){
+    foreach ($permission in ($permissionsList | Sort-Object)){
         $permissionEnabled = $false
-        if($permissionObject.Name -eq "Administrators"){
-            $permissionEnabled = $true
-        }
-        if($role.permissions -contains $permission){
+        if($permissionObject.Name -eq "Administrators" -or $role.permissions -contains $permission){
             $permissionEnabled = $true
         }
         $permissionObject | Add-Member -MemberType NoteProperty -Name $permission -Value $permissionEnabled
@@ -33,7 +28,5 @@ foreach ($role in $roles){
     $formattedRoles += $permissionObject
 }
 
-$formattedRoles = $formattedRoles | %{$obj = new-object psobject; $_.psobject.properties | Sort Name | %{Add-Member -Inp $obj NoteProperty $_.Name $_.Value}; $obj}
 
-     
-$formattedRoles | ConvertTo-Csv | Out-File -FilePath NetWitnessRoles.csv
+$formattedRoles | Export-Csv -Path ./NetWitnessRoles.csv
--- getRoles.js
+++
@@ -1,5 +0,0 @@
-db = db.getSiblingDB('security-server')
-
-print("CUT_TO_HERE")
-
-printjson(  db.getCollection('role').find({  }, { _id: 1, permissions: 1}).toArray() );
